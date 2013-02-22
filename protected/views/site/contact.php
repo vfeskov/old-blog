76e5ff2eeb46950407770af5305b94b3c871371f
Vladimir Feskov <v@vladimirfeskov.com>
2013-02-23 00:53:08 +0200

    contact links are now in one file + grammar fix on contact page

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -11,12 +11,7 @@ $this->breadcrumbs=array(
 
 <h1>How to contact me</h1>
 
-<ul class="contacts">
-    <li><a class="skype" href='skype:vfeskov1' title="Skype me">vfeskov1</a></li>
-    <li><a class="email" title="Email me" href='mai&#108;to&#58;&#118;&#64;%76%6C%61%6&#52;&#105;&#109;irfes&#107;%6Fv&#46;c&#37;6Fm'>v&#64;vladimirfeskov&#46;com</a></li>
-    <li><a class="linkedin" target="_blank" title="Check my profile" href='http://www.linkedin.com/profile/view?id=192299155'>http://www.linkedin.com/profile/view?id=192299155</a></li>
-
-</ul>
+<?php require(dirname(__FILE__).'/contacts.php')?>
 
 <?php if(Yii::app()->user->hasFlash('contact')): ?>
 
@@ -41,7 +36,7 @@ $this->breadcrumbs=array(
 
 	<?php echo $form->errorSummary($model); ?>
 
-    <p>You can write me something right here. Please don't forget to include your contact info, if you want a response.</p>
+    <p>You can write me something right here. Please don't forget to include your contact info if you want a response.</p>
 
     <?php echo $form->textAreaRow($model,'body',array('rows'=>6, 'class'=>'span6')); ?>
 
