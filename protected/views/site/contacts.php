013ddf1e3ece11cb7934764ff03b823fd78f2b86
Vladimir Feskov <v@vladimirfeskov.com>
2013-03-26 01:32:07 +0200

    changed linkedin link

--- protected/views/site/contacts.php
+++ protected/views/site/contacts.php
@@ -1,5 +1,5 @@
 <ul class="contacts">
     <li><a class="skype" href='skype:vfeskov1' title="Skype me">vfeskov1</a></li>
     <li><a class="email" title="Email me" href='mai&#108;to&#58;&#118;&#64;%76%6C%61%6&#52;&#105;&#109;irfes&#107;%6Fv&#46;c&#37;6Fm'>v&#64;vladimirfeskov&#46;com</a></li>
-    <li><a class="linkedin" target="_blank" title="Check my profile" href='http://www.linkedin.com/profile/view?id=192299155'>http://www.linkedin.com/profile/view?id=192299155</a></li>
+    <li><a class="linkedin" target="_blank" title="Check my profile" href="http://ua.linkedin.com/pub/vladimir-feskov/54/498/297">http://www.linkedin.com/profile/view?id=192299155</a></li>
 </ul>
